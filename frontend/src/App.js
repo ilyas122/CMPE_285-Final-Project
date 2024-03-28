cea5386af5b6b766797e6bac45ac019f0b99904f
M.V.N.S.Prudhvi <34057003+prudhvimvns@users.noreply.github.com>
2024-03-27 23:30:40 -0700

    Login component and dashboard component commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Pages/LoginComponent/Login';
+import Dashboard from './Pages/DashboardComponent/Dashboard';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+
+    <Router>
+      <Routes>
+        <Route path="/" element = {<Login/>}></Route>
+        <Route path="/dashboard" element = {<Dashboard/>}></Route>
+
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/Pages/LoginComponent/Login.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./Login.css";
+// import {} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+function Login() {
+    const navigate = useNavigate();
+
+
+const handleDashboardRoute = () =>{
+    navigate('/dashboard');
+}
+
+  return (
+    <div>
+      <div className="container LoginOutlier">
+        <div className="row">
+            <div className="col leftLoginOutlier">
+            </div>
+            <div className="col RightLoginOutlier">
+                <div className="container loginContainer">
+                <div className="loginBox">
+                    <form>
+                    <h1>Login</h1>
+                        <div>
+                        <label htmlFor="username" className="mb-2">Username: </label>
+                        <input type="text" id="username" className="border border-gray-300 p-2 rounded mb-4" />
+                        </div>
+                        <div>
+                        <label htmlFor="password" className="mb-2">Password: </label>
+                        <input type="text" id="password" className="border border-gray-300 p-2 rounded mb-4" />
+                        </div>
+                        <button type="submit" className="btn btn-primary" onClick={handleDashboardRoute}>Login</button>
+                    </form>
+
+                </div>
+
+                </div>
+
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
