df5a50032c3c5c60470d8587720bfbb5cdb4778c
GautamPradyumna <pradyumnavgs@gmail.com>
2024-04-19 16:20:21 -0700

    Added doctor dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,33 +4,16 @@ import Login from './Pages/LoginComponent/Login';
 import Dashboard from './Pages/DashboardComponent/Dashboard';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ConsultDoctor from './Pages/ConsultDoctorComponent/ConsultDoctor';
+import DoctorDashboard from './Pages/DoctorDashboardComponent/DoctorDashboard';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-
-
     <Router>
       <Routes>
         <Route path="/" element = {<Login/>}></Route>
         <Route path="/dashboard" element = {<Dashboard/>}></Route>
         <Route path="/consultdoctor" element = {<ConsultDoctor/>}></Route>
-
+        <Route path="/doctordashboard" element = {<DoctorDashboard/>}></Route>
       </Routes>
     </Router>
   );
