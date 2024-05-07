c58480790e64ff1cbb94a98f4e356268df399afe
GautamPradyumna <pradyumnavgs@gmail.com>
2024-05-06 19:06:38 -0700

    Updating URLS

--- frontend/src/Pages/DashboardComponent/Dashboard.js
+++ frontend/src/Pages/DashboardComponent/Dashboard.js
@@ -154,7 +154,7 @@ const isValidApiKey = (apiKey) => {
     const symptomsArray = selected.map(symptom => parseInt(symptom.id));
     try {
         //const symptomsArray = symptoms.split(',').map(symptom => parseInt(symptom.trim()));
-        const response = await axios.post('http://localhost:5000/predict', { symptoms: symptomsArray });
+        const response = await axios.post(`${DOMAIN}/predict`, { symptoms: symptomsArray });
         const data = response.data;
         console.log('Response:', data);
         setPrediction(data.prediction);
@@ -170,7 +170,7 @@ const handleDisease = async (e) => {
       const userEmail = localStorage.getItem("email");
       const disease = { disease: prediction };
       //const symptomsArray = symptoms.split(',').map(symptom => parseInt(symptom.trim()));
-      const response = await axios.post(`http://localhost:5000/user/add_disease/${userEmail}`,disease);
+      const response = await axios.post(`${DOMAIN}/user/add_disease/${userEmail}`,disease);
       //const data = response.data;
       console.log('Response:', response.data);
   } catch (error) {
@@ -182,7 +182,7 @@ const handleDisease = async (e) => {
     try {
       console.log("KEY:::", apiKey);
       console.log("INPUTTT:::", userInput);
-      const res = await axios.post("http://localhost:5000/ask", {
+      const res = await axios.post(`${DOMAIN}/ask`, {
         openai_api_key: apiKey,
         user_input: userInput,
       });
@@ -203,7 +203,7 @@ const handleDisease = async (e) => {
 
   const getChatHistory = async () => {
     try {
-      const res = await axios.get("http://localhost:5000/get_chat_history");
+      const res = await axios.get(`${DOMAIN}/get_chat_history`);
       setChatHistory(res.data);
     } catch (error) {
       console.error("Error fetching chat history:", error);
